6ba05103cf2e43d3cd9984a9e51e6cea501c8280
Victor Perez <vperezflores2002@gmail.com>
2024-01-02 10:38:36 -0500

    Fixed JS for menu toggle bar for smaller devices and base project is now complete. Begining customization.

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -3,11 +3,11 @@
 const selectElement = selector => {
     const element = document.querySelector(selector)
     if(element) return element;
-    throw new Error(`Something went, make sure that ${selector} exists 
-    or is typed correctly.`);
+    throw new Error(`Something went, make sure that ${selector} exists or is typed correctly.`);
 };
 
 //Nav styles on scroll
+
 const scrollHeader = () => {
     const headerElement = selectElement('#header');
     if(this.scrollY >= 15){
@@ -23,10 +23,9 @@ window.addEventListener('scroll', scrollHeader);
 const menuToggleIcon = selectElement('#menu-toggle-icon');
 
 const toggleMenu = () => {
-    const mobileMenu = selectElement('menu-toggle-icon');
+    const mobileMenu = selectElement('#menu');
     mobileMenu.classList.toggle('activated');
     menuToggleIcon.classList.toggle('activated');
-
 
 };
 
